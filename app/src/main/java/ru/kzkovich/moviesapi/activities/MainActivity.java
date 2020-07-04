1ec528a19d73bfa8d19f135f516f65faad8af503
Kzkovich <kzkovich@gmail.com>
2020-07-04 23:24:17 +0300

    Done with it

---
+++ app/src/main/java/ru/kzkovich/moviesapi/activities/MainActivity.java
@@ -0,0 +1,88 @@
+package ru.kzkovich.moviesapi.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.DownloadManager;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import ru.kzkovich.moviesapi.R;
+import ru.kzkovich.moviesapi.data.MovieAdapter;
+import ru.kzkovich.moviesapi.model.Movie;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+
+        getMovies();
+    }
+
+    private void getMovies() {
+        String url = "http://www.omdbapi.com/?s=batman&apikey=ded7b1aa";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("Search");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        String title = jsonObject.getString("Title");
+                        String year = jsonObject.getString("Year");
+                        String posterUrl = jsonObject.getString("Poster");
+
+                        Movie movie = new Movie();
+                        movie.setTitle(title);
+                        movie.setYear(year);
+                        movie.setPoster(posterUrl);
+                        movies.add(movie);
+                    }
+
+                    movieAdapter = new MovieAdapter(MainActivity.this, movies);
+                    recyclerView.setAdapter(movieAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
+    }
+}
---
+++ app/src/main/java/ru/kzkovich/moviesapi/model/Movie.java
@@ -0,0 +1,35 @@
+package ru.kzkovich.moviesapi.model;
+
+public class Movie {
+
+    private String title;
+    private String year;
+    private String poster;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
